7c29b7350667f07328a55909c1e8af597316fc31
Felix Stürmer <felix.stuermer@elastic.co>
2026-05-21 16:36:29 +0200

    Fix direnv config escaping

--- home-manager-modules/shell.nix
+++ home-manager-modules/shell.nix
@@ -67,15 +67,15 @@ in
 
     # This replaces the content of ~/.config/direnv/direnvrc
     stdlib = ''
-      : "$\{XDG_CACHE_HOME:="$\{HOME}/.cache"}"
+      : "''${XDG_CACHE_HOME:="''${HOME}/.cache"}"
       declare -A direnv_layout_dirs
       
       direnv_layout_dir() {
         local hash path
-        echo "$\{direnv_layout_dirs[$PWD]:=$(
+        echo "''${direnv_layout_dirs[$PWD]:=$(
           hash="$(sha1sum - <<< "$PWD" | head -c40)"
-          path="$\{PWD//[^a-zA-Z0-9]/-}"
-          echo "$\{XDG_CACHE_HOME}/direnv/layouts/$\{hash}$\{path}"
+          path="''${PWD//[^a-zA-Z0-9]/-}"
+          echo "''${XDG_CACHE_HOME}/direnv/layouts/''${hash}''${path}"
         )}"
       }
     '';
