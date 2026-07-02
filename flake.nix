068c76d60bb368b0bff6b92847ef50e222502f8b
Felix Stürmer <weltenwort@users.noreply.github.com>
2026-07-02 16:30:42 +0200

    Fix outputs

--- flake.nix
+++ flake.nix
@@ -11,38 +11,41 @@
 
   outputs =
     { nixpkgs, ... }@inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        darwinModules = {
-          base = import ./darwin-modules/base.nix;
-        };
-
-        homeModules = {
-          claude-code = import ./home-manager-modules/claude-code.nix;
-          devenv = import ./home-manager-modules/devenv.nix;
-          git = import ./home-manager-modules/git.nix;
-          herdr = import ./home-manager-modules/herdr.nix;
-          lima = import ./home-manager-modules/lima.nix;
-          nix = import ./home-manager-modules/nix.nix;
-          opencode = import ./home-manager-modules/opencode.nix;
-          python = import ./home-manager-modules/python.nix;
-          shell = import ./home-manager-modules/shell.nix;
-          ssh = import ./home-manager-modules/ssh.nix;
-        };
-
-        devShells = {
-          default = pkgs.mkShell {
-            buildInputs = [
-              # pkgs.mcp-nixos
-              pkgs.nil
-              pkgs.nixfmt
-            ];
+    inputs.flake-utils.lib.eachDefaultSystem
+      (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          devShells = {
+            default = pkgs.mkShell {
+              buildInputs = [
+                # pkgs.mcp-nixos
+                pkgs.nil
+                pkgs.nixfmt
+              ];
+            };
           };
-        };
-      }
-    );
+        }
+      ) // {
+
+      darwinModules = {
+        base = import ./darwin-modules/base.nix;
+      };
+
+      homeModules = {
+        claude-code = import ./home-manager-modules/claude-code.nix;
+        devenv = import ./home-manager-modules/devenv.nix;
+        git = import ./home-manager-modules/git.nix;
+        herdr = import ./home-manager-modules/herdr.nix;
+        lima = import ./home-manager-modules/lima.nix;
+        nix = import ./home-manager-modules/nix.nix;
+        opencode = import ./home-manager-modules/opencode.nix;
+        python = import ./home-manager-modules/python.nix;
+        shell = import ./home-manager-modules/shell.nix;
+        ssh = import ./home-manager-modules/ssh.nix;
+      };
+
+    };
 }
