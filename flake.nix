ce300f0eba5577074f1171280d5eb5a647211102
Felix Stürmer <weltenwort@users.noreply.github.com>
2026-07-02 16:09:17 +0200

    Add devshell

--- flake.nix
+++ flake.nix
@@ -3,38 +3,45 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    # dot-nix-vim = {
-    #   url = "github:weltenwort/dot-nix-vim";
-    #   # inputs.nixpkgs.follows = "nixpkgs";
-    # };
+  };
+
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
   };
 
   outputs =
-    inputs@{ ... }:
-    {
-      darwinModules = {
-        base = import ./darwin-modules/base.nix;
-      };
+    { nixpkgs, ... }@inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        darwinModules = {
+          base = import ./darwin-modules/base.nix;
+        };
 
-      homeModules = {
-        claude-code = import ./home-manager-modules/claude-code.nix;
-        devenv = import ./home-manager-modules/devenv.nix;
-        git = import ./home-manager-modules/git.nix;
-        lima = import ./home-manager-modules/lima.nix;
-        nix = import ./home-manager-modules/nix.nix;
-        opencode = import ./home-manager-modules/opencode.nix;
-        python = import ./home-manager-modules/python.nix;
-        shell = import ./home-manager-modules/shell.nix;
-        ssh = import ./home-manager-modules/ssh.nix;
-        # nvim = import ./home-manager-modules/nvim.nix;
-        # nvim =
-        #   { ... }:
-        #   {
-        #     imports = [
-        #       inputs.dot-nix-vim.homeModule
-        #       (import ./home-manager-modules/nvim.nix)
-        #     ];
-        #   };
-      };
-    };
+        homeModules = {
+          claude-code = import ./home-manager-modules/claude-code.nix;
+          devenv = import ./home-manager-modules/devenv.nix;
+          git = import ./home-manager-modules/git.nix;
+          lima = import ./home-manager-modules/lima.nix;
+          nix = import ./home-manager-modules/nix.nix;
+          opencode = import ./home-manager-modules/opencode.nix;
+          python = import ./home-manager-modules/python.nix;
+          shell = import ./home-manager-modules/shell.nix;
+          ssh = import ./home-manager-modules/ssh.nix;
+        };
+
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = [
+              # pkgs.mcp-nixos
+              pkgs.nil
+              pkgs.nixfmt
+            ];
+          };
+        };
+      }
+    );
 }
