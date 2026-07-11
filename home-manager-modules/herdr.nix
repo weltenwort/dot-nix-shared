71116ba3f3177953f24931e49c8bf7a826aa6096
Felix Stürmer <weltenwort@users.noreply.github.com>
2026-07-11 13:39:34 +0000

    Add herdr config

--- home-manager-modules/herdr.nix
+++ home-manager-modules/herdr.nix
@@ -1,6 +1,19 @@
 { pkgs, ... }:
 {
-  home.packages = [
-    pkgs.herdr
-  ];
+  programs.herdr = {
+    enable = true;
+    settings = {
+      onboarding = false;
+      theme = {
+        name = "one-dark";
+        auto_switch = false;
+      };
+      ui = {
+        show_agent_labels_on_pane_borders = true;
+        toast.delivery = "terminal";
+        sound.enabled = false;
+      };
+      keys.prefix = "ctrl+a";
+    };
+  };
 }
